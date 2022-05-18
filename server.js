b1ddfea5c789f8a3849e886cc9de8ade68b5273b
Matias Hospital <matiashospital@gmail.com>
2022-05-18 19:42:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('/dist/paises'));
 
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
   res.sendFile(path.join(__dirname,'/dist/paises/index.html'));
 });
 
