2c63622d3c763a5b5dc42c79f8c9f64868d58e65
Matias Hospital <matiashospital@gmail.com>
2022-05-18 19:48:01 -0300

    .

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('/dist/paises'));
 
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname,'/dist/paises/index.html'));
+  res.sendFile(path.join(__dirname,'/dist'));
 });
 
 // Start the app by listening on the default Heroku port
